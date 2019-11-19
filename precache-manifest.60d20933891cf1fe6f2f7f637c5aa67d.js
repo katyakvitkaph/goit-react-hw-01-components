423dbc50e65040090e951f8b33ed40d856c96baf
katyakvitkaph <kvitka.katja@gmail.com>
2019-11-19 12:40:56 +0200

    Updates

---
+++ precache-manifest.60d20933891cf1fe6f2f7f637c5aa67d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3332d448f76495c89bfe9309dd9de6d8",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "320d643a22b4960baaa4",
+    "url": "/goit-react-hw-01-components/static/css/main.26af0cb1.chunk.css"
+  },
+  {
+    "revision": "72509fa2674d7d8c60a6",
+    "url": "/goit-react-hw-01-components/static/js/2.9b7975e6.chunk.js"
+  },
+  {
+    "revision": "320d643a22b4960baaa4",
+    "url": "/goit-react-hw-01-components/static/js/main.0d220ef3.chunk.js"
+  },
+  {
+    "revision": "76c0918716ad9d7cd3e5",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.17fd7762.js"
+  }
+]);
